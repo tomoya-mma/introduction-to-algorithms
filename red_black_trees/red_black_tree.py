0e25976c8fa5429f4466552ba3a671d663a1827e
Tomoya Sasaki <tomoya.rockin.all.over.the.world@gmail.com>
2024-03-24 12:17:24 +0900

    Implement remove method

--- red_black_trees/red_black_tree.py
+++ red_black_trees/red_black_tree.py
@@ -64,21 +64,21 @@ class Tree:
 
     def contains(self, key):
         p = self.root
-        while p and p.key != key:
+        while p != nil() and p.key != key:
             p = p.left if key < p.key else p.right
 
         return p
 
     def successor(self, x):
-        if x.right:
+        if x.right != nil():
             p = x.right
-            while p.left:
+            while p.left != nil():
                 p = p.left
 
             return p
         else:
             p, q = x, x.parent
-            while q and p == q.right:
+            while q != nil() and p == q.right:
                 p = q
                 q = q.parent
 
@@ -87,20 +87,33 @@ class Tree:
     def remove(self, z):
         self.size -= 1
 
-        if not z.left:
+        y = z
+        color_of_y = y.color
+
+        if z.left == nil():
+            x = z.right
             self.__transplant(z, z.right)
-        elif not z.right:
+        elif z.right == nil():
+            x = z.left
             self.__transplant(z, z.left)
         else:
             y = self.successor(z)
+            color_of_y = y.color
+            x = y.right
             if y.parent != z:
                 self.__transplant(y, y.right)
                 y.right = z.right
                 y.right.parent = y
+            else:
+                x.parent = y
 
             self.__transplant(z, y)
             y.left = z.left
             y.left.parent = y
+            y.color = z.color
+
+        if color_of_y == Color.BLACK:
+            self.__fix_after_deletion(x)
 
         self.height = self.__get_height()
 
@@ -196,15 +209,73 @@ class Tree:
         self.root.color = Color.BLACK
 
     def __transplant(self, p, q):
-        if not p.parent:
+        if p.parent == nil():
             self.root = q
         elif p == p.parent.left:
             p.parent.left = q
         else:
             p.parent.right = q
 
-        if q:
-            q.parent = p.parent
+        q.parent = p.parent
+
+    def __fix_after_deletion(self, x):
+        while x != self.root and x.color == Color.BLACK:
+            if x == x.parent.left:
+                w = x.parent.right
+                if w.color == Color.RED:
+                    # case 1
+                    x.parent.color = Color.RED
+                    w.color = Color.BLACK
+                    self.__rotate_left(x.parent)
+                    w = x.parent.right
+
+                if w.left.color == Color.BLACK and w.right.color == Color.BLACK:
+                    # case 2
+                    w.color = Color.RED
+                    x = x.parent
+                else:
+                    if w.right.color == Color.BLACK:
+                        # case 3
+                        w.left.color = Color.BLACK
+                        w.color = Color.RED
+                        self.__rotate_right(w)
+                        w = x.parent.right
+
+                    # case 4
+                    w.color = x.parent.color
+                    x.parent.color = Color.BLACK
+                    w.right.color = Color.BLACK
+                    self.__rotate_left(x.parent)
+                    x = self.root
+            else:
+                w = x.parent.right
+                if w.color == Color.RED:
+                    # case 1
+                    x.parent.color = Color.RED
+                    w.color = Color.BLACK
+                    self.__rotate_right(x.parent)
+                    w = x.parent.left
+
+                if w.right.color == Color.BLACK and w.left.color == Color.BLACK:
+                    # case 2
+                    w.color = Color.RED
+                    x = x.parent
+                else:
+                    if w.left.color == Color.BLACK:
+                        # case 3
+                        w.right.color = Color.BLACK
+                        w.color = Color.RED
+                        self.__rotate_left(w)
+                        w = x.parent.left
+
+                    # case 4
+                    w.color = x.parent.color
+                    x.parent.color = Color.BLACK
+                    w.left.color = Color.BLACK
+                    self.__rotate_right(x.parent)
+                    x = self.root
+
+        x.color = Color.BLACK
 
     def __get_height(self):
 
@@ -229,6 +300,15 @@ class Tree:
 if __name__ == '__main__':
     t = Tree()
 
+    t.put(Element(2))
+    t.put(Element(1))
+    t.put(Element(4))
+    t.put(Element(5))
+    t.put(Element(9))
+    t.put(Element(3))
+    t.put(Element(6))
+    t.put(Element(7))
+
     while True:
         print("1:put 2:remove 3:print 4:successor > ", end='')
         op = int(input())
@@ -243,7 +323,7 @@ if __name__ == '__main__':
             key = int(input())
 
             z = t.contains(key)
-            if not z:
+            if z == nil():
                 print(f"{key} is not found in the tree.")
             else:
                 t.remove(z)
@@ -254,11 +334,11 @@ if __name__ == '__main__':
             key = int(input())
 
             z = t.contains(key)
-            if not z:
+            if z == nil():
                 print(f"{key} is not found in the tree.")
             else:
                 succ = t.successor(z)
-                if not succ:
+                if succ == nil():
                     print("successor is None.")
                 else:
                     print(f"successor = {succ.key}")
